dd508ff5582a63372c6659ad0623946b162aeebd
valelv02 <valeria.londonove@upb.edu.co>
2026-07-28 13:35:01 -0500

    Mi primer commit

---
+++ 1.VariablesConstantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace _1.VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre= "Valeria";
+            Console.WriteLine("Bienvenida "+ nombre + " al curso Fundamentos de Programación");
+        }
+    }
+}
