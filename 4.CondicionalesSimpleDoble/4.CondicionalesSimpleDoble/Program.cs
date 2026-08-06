758070eaaeb6e46450e96567304b53c721634476
valelv02 <valeria.londonove@upb.edu.co>
2026-08-06 13:22:07 -0500

    Condicionales simples dobles

---
+++ 4.CondicionalesSimpleDoble/4.CondicionalesSimpleDoble/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.CondicionalesSimpleDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////ESTRUCTURAS DE CONTROL CONDICIONAL
+            //Condicional Simple 
+            //1.   Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: "Bienvenido a mi sitio web"
+
+            /*byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToByte(Console.ReadLine());
+            if (edad>=18)
+              {
+
+                //Si se cumple la condición
+                  Console.WriteLine("Bienvenido a mi sitio web");*/
+
+            //Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos
+            /*string nombre;
+            float sueldo = 0;
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su sueldo:");
+            sueldo = Convert.ToSingle(Console.ReadLine());
+            if (sueldo >= 3000)
+            {
+                Console.WriteLine("Usted " + nombre + " debe abonar impuestos");
+            }*/
+
+
+            //CONDICIONALES DOBLES
+            //Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: "Bienvenido a mi sitio web". De lo contrario, mostrar el mensaje " No es apto para el contenido dde este sitio web"
+
+            byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToByte(Console.ReadLine());
+            if (edad>=18)
+            {
+
+                //Si se cumple la condición
+                  Console.WriteLine("Bienvenido a mi sitio web");
+            }
+            else
+            {
+                //Si NO cumple la condición
+                Console.WriteLine("No es apto para el contenido dde este sitio web");
+            }
+        }
+
+    }
+}
