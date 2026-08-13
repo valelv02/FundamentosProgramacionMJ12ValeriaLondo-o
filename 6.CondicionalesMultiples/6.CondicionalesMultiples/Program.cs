566ae41ceebb0598fbc9f39ed253d0531305d6f0
valelv02 <valeria.londonove@upb.edu.co>
2026-08-13 13:27:48 -0500

    6. Condicionales Múltiples

--- 6.CondicionalesMultiples/6.CondicionalesMultiples/Program.cs
+++ 6.CondicionalesMultiples/6.CondicionalesMultiples/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace _6.CondicionalesMultiples
 {
@@ -10,6 +7,41 @@ namespace _6.CondicionalesMultiples
     {
         static void Main(string[] args)
         {
+            //Condicionales Múltiples
+            // Crear un algoritmo que muestre por pantalla un menú con 5 opciones, y según la opción elegida del menú, muestre un mensaje por pantalla donde se muestre la opción elegida, en casp de que se selccione una opción que no está en el menú mostrar un mensaje de error.
+
+            int respuesta = 0;
+            Console.WriteLine("--------Menú--------");
+            Console.WriteLine("1.Opción 1       2.Opción 2");
+            Console.WriteLine("3.Opción 3       4.Opción 4");
+            Console.WriteLine("5.Opción 5");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Elija una opción del menú");
+            respuesta = int.Parse(Console.ReadLine());
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Eligió la opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligió la opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligió la opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligió la opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligió la opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción válida");
+                    break;
+            }
         }
     }
 }
+    
+
