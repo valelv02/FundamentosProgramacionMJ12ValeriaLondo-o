fceaeabf9598df3c673695e3b9d52947d3e2527c
valelv02 <valeria.londonove@upb.edu.co>
2026-08-26 21:29:54 -0500

    Ejercicio en casa ciclo while

---
+++ 8. CicloWhile1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8.CicloWhile1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int acumulador = 0;
+
+            Console.WriteLine("Ingresa un número entero positivo para sumar (o un negativo para salir):");
+            int numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                acumulador += numero;
+
+                Console.WriteLine("Ingresa otro número:");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"La suma total de los números positivos ingresados es: {acumulador}");
+        }
+    }
+}
