05f1671b469070f3b7c1938fc0a3b917cf6e85ef
valelv02 <valeria.londonove@upb.edu.co>
2026-08-31 19:17:27 -0500

    Actividad en casa Ciclo While

---
+++ 10.CiclosWhile3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _10.CiclosWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Random random = new Random();
+            int numeroSecreto = random.Next(1, 101); 
+
+            Console.WriteLine("Adivina el número secreto entre 1 y 100:");
+            int intento = int.Parse(Console.ReadLine());
+
+            while (intento != numeroSecreto)
+            {
+                if (intento > numeroSecreto)
+                {
+                    Console.WriteLine("Demasiado alto. Intenta de nuevo:");
+                }
+                else
+                {
+                    Console.WriteLine("Demasiado bajo. Intenta de nuevo:");
+                }
+
+                intento = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("¡Correcto! Has adivinado el número secreto.");
+        }
+    }
+}
